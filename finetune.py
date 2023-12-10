026d95c4058eec120ea4d77b28bd8663a5dc4337
geronimi73 <141400217+geronimi73@users.noreply.github.com>
2023-12-10 01:35:59 +0100

    monkeypatch MambaLMHeadModel.forward()

--- finetune.py
+++ finetune.py
@@ -1,23 +1,52 @@
-import torch
-import os
-import wandb
-from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer, BitsAndBytesConfig
-from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
+import torch, os, wandb
+from transformers import AutoTokenizer, TrainingArguments, Trainer
 from accelerate import Accelerator
-from datasets import load_dataset, DatasetDict, Dataset
+from datasets import load_dataset
 
 from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
 
+# monkey patch MambaLMHeadModel.forward 
+def forward_with_loss(self, input_ids, position_ids=None, inference_params=None, num_last_tokens=0, labels = None):
+    """
+    "position_ids" is just to be compatible with Transformer generation. We don't use it.
+    num_last_tokens: if > 0, only return the logits for the last n tokens
+    """
+    hidden_states = self.backbone(input_ids, inference_params=inference_params)
+    if num_last_tokens > 0:
+        hidden_states = hidden_states[:, -num_last_tokens:]
+    lm_logits = self.lm_head(hidden_states)
+    
+    # Source: https://github.com/huggingface/transformers/blob/80377eb018c077dba434bc8e7912bcaed3a64d09/src/transformers/models/llama/modeling_llama.py#L1196
+    from torch.nn import CrossEntropyLoss
+    if labels is not None:
+        logits = lm_logits
+        # Shift so that tokens < n predict n
+        shift_logits = logits[..., :-1, :].contiguous()
+        shift_labels = labels[..., 1:].contiguous()
+        # Flatten the tokens
+        loss_fct = CrossEntropyLoss()
+        # shift_logits = shift_logits.view(-1, self.config.vocab_size)
+        shift_logits = shift_logits.view(-1, self.backbone.embedding.weight.size()[0])
+        shift_labels = shift_labels.view(-1)
+        # Enable model parallelism
+        shift_labels = shift_labels.to(shift_logits.device)
+        loss = loss_fct(shift_logits, shift_labels)
+        return (loss,)   
+    else:
+        CausalLMOutput = namedtuple("CausalLMOutput", ["logits"])
+        return CausalLMOutput(logits=lm_logits)
+MambaLMHeadModel.forward=forward_with_loss
+
 accelerator = Accelerator()
+wandb.init(mode="disabled")
 
-# wandb.init(mode="disabled")
-
-modelpath="state-spaces/mamba-130m"
+modelpath="state-spaces/mamba-1.4b"
 
 # Load model
 model = MambaLMHeadModel.from_pretrained(
     modelpath,    
     dtype=torch.bfloat16,
+    device="cuda",
 )
 
 # Load Tokenizer
@@ -25,14 +54,15 @@ tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
 tokenizer.pad_token = tokenizer.eos_token
 
 # Load dataset
-dataset = load_dataset("OpenAssistant/oasst_top1_2023-08-25")
+dataset_name="OpenAssistant/oasst_top1_2023-08-25"
+dataset=load_dataset(dataset_name)
 
 # Tokenize dataset
 def tokenize(element):
     return tokenizer(
         element["text"],
         truncation=True,
-        max_length=512,
+        max_length=1024,
         add_special_tokens=False,
     )
 
@@ -48,26 +78,31 @@ def collate(elements):
     tokenlist=[e["input_ids"] for e in elements]
     tokens_maxlen=max([len(t) for t in tokenlist])
 
-    input_ids,labels,attention_masks = [],[],[]
+    input_ids,labels = [],[]
     for tokens in tokenlist:
         pad_len=tokens_maxlen-len(tokens)
 
-        # pad input_ids with pad_token, labels with ignore_index (-100) and set attention_mask 1 where content otherwise 0
+        # pad input_ids with pad_token, labels with ignore_index (-100) 
         input_ids.append( tokens + [tokenizer.pad_token_id]*pad_len )   
         labels.append( tokens + [-100]*pad_len )    
-        attention_masks.append( [1]*len(tokens) + [0]*pad_len ) 
-
     batch={
         "input_ids": torch.tensor(input_ids),
         "labels": torch.tensor(labels),
-        # "attention_mask": torch.tensor(attention_masks)
     }
     return batch
 
-bs=32        # batch size
+bs=4        # batch size
 ga_steps=1  # gradient acc. steps
-epochs=5
+epochs=3
 steps_per_epoch=len(dataset_tokenized["train"])//(accelerator.state.num_processes*bs*ga_steps)
+lr=0.00005
+
+run_name="{model}_{ds}_BS-{bs}_LR-{lr}".format(
+    model=modelpath.split("/")[1],
+    ds=dataset_name.split("/")[1],
+    bs=bs,
+    lr=lr,
+    )
 
 args = TrainingArguments(
     output_dir="out",
@@ -80,13 +115,12 @@ args = TrainingArguments(
     gradient_accumulation_steps=ga_steps,
     num_train_epochs=epochs,
     lr_scheduler_type="constant",
-    # optim="paged_adamw_32bit",
-    learning_rate=0.0005,
+    learning_rate=lr,
     group_by_length=True,
-    fp16=False,
     bf16=True,
-    # ddp_find_unused_parameters=False,
-    save_safetensors=False
+    ddp_find_unused_parameters=False,
+    save_safetensors=False,
+    run_name=run_name
 )
 
 trainer = Trainer(
